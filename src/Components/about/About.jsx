7a4d1a444132503330d3418f1cbfd66477222e66
Zain Hashmi <zainhashmi692@gmail.com>
2024-02-28 21:02:51 +0500

    Social media  icons in-progress

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -1,43 +1,53 @@
-import React from 'react'
-import './about.css'
-import Photo from '../../assets/Zain.jpg'
+import React from "react";
+import "./about.css";
+import Photo from "../../assets/Zain.jpg";
+import github from "../../assets/github_mark.png";
+import facebook from "../../assets/facebook_icon.png";
 const About = () => {
   return (
-    <div className='about__section'>
-      <div className='about_section_image'>
-        <img src={Photo} alt='Zain' />
+    <div className="about__section">
+      <div className="about_section_image">
+        <img src={Photo} alt="Zain" />
       </div>
-      <div className='about__section__description'>
+      <div className="about__section__description">
         <h3>About Me</h3>
         <h1>Zain Hashmi</h1>
         <p>
-          Currently, I am pursuing a degree in Software Engineering from Comsats
-          University Islamabad. With a strong skill set and the urge to learn
-          new things, I want to be a part of the industry where I can utiize my
-          skill and energy to solve real life problems and learn new things.
-          This is my portfolio site to introduce myself.
+          I have been working as a freelancer for the past 2 years. I have
+          worked with clients on different projects. I have worked on different
+          technologies like MERN stack, Python, Java, and many more. I am also
+          working on my personal project{" "}
+          <a href="https://github.com/Z9dot/E-REM">E-REM</a>.
         </p>
-        <div className='my__skills'>
-        <h2>My Skills</h2>
-        <div className='programming'>
-          <button>Python</button>
-          <button>Java</button>
-          <button>HTML</button>
-          <button>CSS</button>
-          <button>JavaScript</button>
+        <div className="my__skills">
+          <h2>My Skills</h2>
+          <div className="programming">
+            <button>Python</button>
+            <button>Java</button>
+            <button>HTML</button>
+            <button>CSS</button>
+            <button>JavaScript</button>
           </div>
-        <div className='mern__stack'>
-          <button>MongoDB</button>
-          <button>Express</button>
-          <button>React</button>
-          <button>Node</button>
+          <div className="mern__stack">
+            <button>MongoDB</button>
+            <button>Express</button>
+            <button>React</button>
+            <button>Node</button>
           </div>
-          
+        </div>
+        <div className="social__media__accounts">
+          <h2>Connect with me</h2>
+          <div className="social__media__icons">
+            <div>
+              <a href="githu">
+                <i src="{github}" alt="Github"/>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-      </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
